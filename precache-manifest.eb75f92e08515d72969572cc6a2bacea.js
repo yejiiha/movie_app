7028b601c02dbefd4e1dcd5aee1a1c5a3eaf0981
yezii <1126yezy@gmail.com>
2020-08-24 13:05:52 +0900

    Updates

---
+++ precache-manifest.eb75f92e08515d72969572cc6a2bacea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadc00aa62dc2f48431115134f4a3044",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b1b51268c2dd9eff7332",
+    "url": "/movie_app/static/css/main.ebc79f17.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b51268c2dd9eff7332",
+    "url": "/movie_app/static/js/main.5ba19ddf.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
