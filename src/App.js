8665bf185b38c38dcbb802a4d254d971d9eeeda6
yezii <1126yezy@gmail.com>
2020-08-24 13:24:35 +0900

    Creating a movie app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "pizza",
-    image:
-      "https://www.hapskorea.com/wp-content/uploads/2019/08/octopusbomb-pizza.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "sashimi",
-    image:
-      "https://casasensei.com/wp-content/uploads/2019/07/sushi-vs-sashimi.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 3,
-    name: "pasta",
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/pasta-salad-horizontal-jpg-1522172096.jpg",
-    rating: 4.6,
-  },
-  {
-    id: 4,
-    name: "beef",
-    image:
-      "https://www.thespruceeats.com/thmb/5cv2uGBNm4qhLIYl9RlefAUq7P0=/4048x2277/smart/filters:no_upscale()/marinated-rump-roast-3058682-hero-01-0977a498722f47debaa7034c13053048.jpg",
-    rating: 4,
-  },
-];
+class App extends React.Component {
+  state = {
+    isLoading: true, // mount되자마자 isLoading은 기본적으로 true
+    movies: [], // 미래에 쓰고자 하는 state 미리 선언한 것
+  };
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-}; //내가 얻고 싶은 props에 대한 설명
+  componentDidMount() {
+    this.getMovies();
+  }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state; // The movies from state
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
